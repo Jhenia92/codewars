595a6c674faeeee60298ab77e99935ef07382c29
E.I.Arabina <E.I.Arabina92@gmail.com>
2025-11-28 14:25:30 +0300

    sum _mixed_array

---
+++ 8kyu/sum _mixed_array.js
@@ -0,0 +1,13 @@
+/* Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number. */
+
+function sumMix(x) {
+    let result=0;
+    for (let i = 0; i < x.length; i++) {
+        result+= Number(x[i]);
+    }
+    return result;
+};
+
+console.log(sumMix(['1', '2', 3, 4]));
